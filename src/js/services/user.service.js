1220fa0febe348c1eb5b210d395058aaf790d454
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-14 19:47:13 +0100

    Getting the user's info on page load

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,11 +1,12 @@
 export default class User {
-  constructor(JWT, AppConstants, $http, $state){
+  constructor(JWT, AppConstants, $http, $state, $q){
     'ngInject';
 
     this._AppConstants = AppConstants;
     this._$http = $http;
     this._JWT = JWT;
     this._$state = $state;
+    this._$q = $q;
 
     //user properties
     this.current = null;
@@ -38,5 +39,39 @@ export default class User {
     this._JWT.destroy();
     //Do a hard reload of current state to ensure all data is flushed
     this._$state.go(this._$state.$current, null, {reload: true});
+  }
+
+  verifyAuth()
+  {
+    let deferred = this._$q.defer();
+
+    //Check for JWT token first
+    if(!this._JWT.get()){
+      deferred.resolve(false);
+      return deferred.promise;
+    }
+
+    //If there's a JWT & user is already set
+    if(this.current){
+      deferred.resolve(true);
+    }else{
+      this._$http({
+        url: this._AppConstants.api + '/user',
+        method: 'GET',
+        headers: {
+          Authorization: 'Token ' + this._JWT.get()
+        }
+      }).then(
+        (res) => {
+          this.current = res.data.user;
+          deferred.resolve(true);
+        },
+        (err) => {
+          this._JWT.destroy();
+          deferred.resolve(false);
+        }
+      );
+    }
+    return deferred.promise;
   }
 }
