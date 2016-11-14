0e104e50576e5eeb4198021fcb380152308ea2a1
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-14 15:22:15 +0100

    Creating a reusable component for form errors

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,5 +2,8 @@ import angular from 'angular';
 
 let componentsModule = angular.module('app.components', []);
 
+//components
+import ListErrors from './list-errors.component';
+componentsModule.component('ListErrors', ListErrors);
 
 export default componentsModule;
