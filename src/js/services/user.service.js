a842d91196baa5628de5e04843ae54fbcd8efd79
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-14 15:58:50 +0100

    Log Out

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,10 +1,11 @@
 export default class User {
-  constructor(JWT, AppConstants, $http){
+  constructor(JWT, AppConstants, $http, $state){
     'ngInject';
 
     this._AppConstants = AppConstants;
     this._$http = $http;
     this._JWT = JWT;
+    this._$state = $state;
 
     //user properties
     this.current = null;
@@ -23,12 +24,19 @@ export default class User {
       (res) => {
         //Set the JWT token
         this._JWT.save(res.data.user.token);
-        
+
         //Store tue user's info for easy lookup
         this.current = res.data.user;
 
         return res;
       }
     );
+  }
+
+  logout(){
+    this.current = null;
+    this._JWT.destroy();
+    //Do a hard reload of current state to ensure all data is flushed
+    this._$state.go(this._$state.$current, null, {reload: true});
   }
 }
