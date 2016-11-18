b3bb91f6c6be8d4a9cc1894002ec4e2e3f8209de
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-18 11:22:09 +0100

    Creating article Editor - Adding and Removing Tags

--- src/js/editor/editor.controller.js
+++ src/js/editor/editor.controller.js
@@ -1,6 +1,24 @@
 class EditorCtrl{
   constructor(){
     'ngInject';
+
+    this.article = {
+      title: '',
+      description: '',
+      body: '',
+      tagList: []
+    }
+  }
+
+  addTag(){
+    if(!this.article.tagList.includes(this.tagField)){
+      this.article.tagList.push(this.tagField);
+      this.tagField = '';
+    }
+  }
+
+  removeTag(tagName){
+    this.article.tagList = this.article.tagList.filter((slug) => slug != tagName);
   }
 }
 
