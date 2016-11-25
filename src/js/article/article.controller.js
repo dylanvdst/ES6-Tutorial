2a30241544db56b3edfa5f6a6206fd71e6a425dd
dylanvdst <dylan.vandesteene.u8267@student.hogent.be>
2016-11-25 14:56:03 +0100

    Creating commenting functionality

--- src/js/article/article.controller.js
+++ src/js/article/article.controller.js
@@ -1,19 +1,49 @@
 import marked from 'marked';
 
 class ArticleCtrl {
-  constructor(article, $sce, $rootScope) {
-    'ngInject';
+    constructor(article, User, Comments, $sce, $rootScope) {
+        'ngInject';
 
-    this.article = article;
+        this.article = article;
+        this.currentUser = User.current;
+        this._Comments = Comments;
 
-    //update the itle of this page
-    $rootScope.setPageTitle(this.article.title);
+        //update the itle of this page
+        $rootScope.setPageTitle(this.article.title);
 
-    //Transform the markdown into html
-    this.article.body = $sce.trustAsHtml(marked(this.article.body, {sanitize: true}));
+        //Transform the markdown into html
+        this.article.body = $sce.trustAsHtml(marked(this.article.body, {
+          sanitize: true
+        }));
+        //Get comments for the article
+        Comments.getAll(this.article.slug).then(
+          (comments) => this.comments = comments
+        );
 
-  }
+        //Initialize blank comment form
+        this.resetCommentForm();
+    }
+
+    resetCommentForm() {
+        this.commentForm = {
+            isSubmitting: false,
+            body: '',
+            errors: []
+        }
+    }
+
+    addComment() {
+      this.commentForm.isSubmitting = true;
+
+  this._Comments.add(this.article.slug, this.commentForm.body)
+    .then(comment => {
+      this.comments.unshift(comment);
+      this.resetCommentForm();
+    }).catch(err => {
+      this.commentForm.isSubmitting = false;
+      this.commentForm.errors = err.data.errors;
+    });
+    }
 }
-
 
 export default ArticleCtrl;
---
+++ src/js/services/comments.service.js
@@ -0,0 +1,27 @@
+export default class Comments{
+  constructor(AppConstants, $http){
+    'ngInject;'
+
+    this._AppConstants = AppConstants;
+    this._$http = $http;
+  }
+
+  add(slug, payload) {
+    return this._$http({
+      url: `${this._AppConstants.api}/articles/${slug}/comments`,
+      method: "POST",
+      data: {
+        comment: {
+          body: payload
+        }
+      }
+    }).then(res => res.data.comment);
+  };
+
+  getAll(slug) {
+    return this._$http({
+      url: `${this._AppConstants.api}/articles/${slug}/comments`,
+      method: "GET"
+    }).then(res => res.data.comments);
+  };
+}
